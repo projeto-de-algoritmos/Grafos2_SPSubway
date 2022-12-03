36199913a618f46e6d8333ecf3a7d55201839a5b
Guilherme Barbosa <mrguilhermeferreira@gmail.com>
2022-12-03 00:52:28 -0300

    feat: add states json and use in select buttons

--- app/src/components/Home.jsx
+++ app/src/components/Home.jsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import { SelectStateButton } from "./SelectStateButton";
 
 export function Home() {
+  const [departure, setDeparture] = useState("");
+  const [arrival, setArrival] = useState("");
+
   return (
     <div className="w-full max-w-md space-y-5">
       <h1 className="font-bold text-4xl">Olá</h1>
@@ -8,9 +12,22 @@ export function Home() {
         Nós iremos escolher o menor caminho da sua viagem.
       </p>
       <p className="text-3xl">Por favor, informe o destino:</p>
-      <SelectStateButton localPoint={"partida"} />
-      <SelectStateButton localPoint={"chegada"} />
-      <button className="w-full bg-green-500 h-[64px] rounded-lg text-white font-bold hover:bg-green-600 transition-colors">
+      <SelectStateButton
+        localPoint={"partida"}
+        onChange={(state) => setDeparture(state)}
+        departure={departure}
+        arrival={arrival}
+      />
+      <SelectStateButton
+        localPoint={"chegada"}
+        onChange={(state) => setArrival(state)}
+        departure={departure}
+        arrival={arrival}
+      />
+      <button
+        className="w-full bg-green-500 h-[64px] rounded-lg text-white font-bold hover:bg-green-600 transition-colors disabled:bg-gray-400"
+        disabled={!departure || !arrival}
+      >
         Encontrar
       </button>
     </div>
